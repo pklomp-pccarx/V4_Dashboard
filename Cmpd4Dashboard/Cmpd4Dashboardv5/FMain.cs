99a35b20f7af28ade9fd214ad4a1e798fa7096c0
Paul Klomp <pklomp@pccarx.com>
2015-07-02 13:20:38 -0500

    no message

---
+++ Cmpd4Dashboard/Cmpd4Dashboardv5/FMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Cmpd4Dashboardv5
+{
+    public partial class FrmMain : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Cmpd4DashboardVeiwerv/Cmpd4DashboardVeiwerv1/FMain.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Cmpd4DashboardVeiwerv1
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.dashboardViewer1 = new DevExpress.DashboardWin.DashboardViewer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardViewer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ribbonControl1
+            // 
+            this.ribbonControl1.ExpandCollapseItem.Id = 0;
+            this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbonControl1.ExpandCollapseItem});
+            this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
+            this.ribbonControl1.MaxItemId = 1;
+            this.ribbonControl1.Name = "ribbonControl1";
+            this.ribbonControl1.RibbonStyle = DevExpress.XtraBars.Ribbon.RibbonControlStyle.Office2010;
+            this.ribbonControl1.Size = new System.Drawing.Size(1062, 49);
+            // 
+            // dashboardViewer1
+            // 
+            this.dashboardViewer1.DashboardSource = new System.Uri("C:\\Users\\pklomp\\Desktop\\V4_DoctorTest.xml", System.UriKind.Absolute);
+            this.dashboardViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dashboardViewer1.Location = new System.Drawing.Point(0, 49);
+            this.dashboardViewer1.Name = "dashboardViewer1";
+            this.dashboardViewer1.PrintingOptions.FontInfo.GdiCharSet = ((byte)(0));
+            this.dashboardViewer1.PrintingOptions.FontInfo.Name = null;
+            this.dashboardViewer1.Size = new System.Drawing.Size(1062, 648);
+            this.dashboardViewer1.TabIndex = 1;
+            // 
+            // FrmMain
+            // 
+            this.AllowFormGlass = DevExpress.Utils.DefaultBoolean.False;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1062, 697);
+            this.Controls.Add(this.dashboardViewer1);
+            this.Controls.Add(this.ribbonControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.Ribbon = this.ribbonControl1;
+            this.Text = "The Compounder 4 - Dashboard viewer";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardViewer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl1;
+        private DevExpress.DashboardWin.DashboardViewer dashboardViewer1;
+    }
+}
+
